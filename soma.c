51a6d47d31c3b8b97d4a23aaddf2ac645a8c7537
Romulo Madureira <84478973+saganet157@users.noreply.github.com>
2021-11-11 23:02:08 -0300

    Create soma.c

---
+++ soma.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+int x, y, soma;
+
+
+        printf("Digite o vlaor de X:\n");
+    scanf("%i",&x);
+
+        printf("Digite o vlaor de Y:\n");
+    scanf("%i",&y);
+
+            soma = x+y;
+
+            printf("SOMA = %i",soma);
+
+    return 0;
+}
