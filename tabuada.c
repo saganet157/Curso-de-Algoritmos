ac9636c92e5da36abaed32c93068a8a046c44af4
Romulo Madureira <84478973+saganet157@users.noreply.github.com>
2022-01-13 17:05:47 -0300

    Create tabuada.c

---
+++ tabuada.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Tabuada
+
+int main()
+{
+    int n, produto;
+
+    printf("Deseja a tabuada para qual valor?\n");
+    scanf("%d", &n);
+
+
+    for (int i=1;i<=10;i++){
+        produto = n * i;
+        printf("%d x %d = %d\n",n,i,produto);
+    }
+
+
+    return 0;
+}
